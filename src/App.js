8fd6235db1cdc94c5130b406f9e2dc6d6a040a1d
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-04-13 01:30:09 +0100

    made many changes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,71 @@
 import './App.css';
 import './style.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router/router';
-import { clusterApiUrl } from "@solana/web3.js";
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import {
-  CoinbaseWalletAdapter,
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  TorusWalletAdapter
-} from '@solana/wallet-adapter-wallets';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { ChakraProvider } from '@chakra-ui/react';
-import { useMemo } from 'react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { useEffect, useState } from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletNotConnectedError } from "@solana/wallet-adapter-base";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import nacl from "tweetnacl";
+import HomePage from './pages/homepage';
 
 
-require("@solana/wallet-adapter-react-ui/styles.css");
 function App() {
-  const network = WalletAdapterNetwork.Devnet;
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  const wallets = useMemo(
-    () => [
-      new CoinbaseWalletAdapter(),
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter({ network }),
-      new TorusWalletAdapter()
-    ],
-    [network]
-  );
-  return (
+  const { publicKey, signMessage, connected } = useWallet();
 
-    <ChakraProvider>
-      <ConnectionProvider endpoint={endpoint}>
-        <WalletProvider wallets={wallets} autoConnect>
-          <WalletModalProvider>
-            <RouterProvider router={router} />
-          </WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider >
-    </ChakraProvider>
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const [signature, setSignature] = useState("");
+  const [signedMessage, setSignedMessage] = useState("");
+  const [verified, setVerified] = useState();
+
+  useEffect(() => {
+    if (!connected) {
+      setError("");
+      setMessage("");
+      setSignature("");
+      setSignedMessage("");
+      setVerified();
+    }
+  }, [connected]);
+
+  const handleInput = (e) => {
+    const msg = e.target.value;
+    setMessage(msg);
+  };
+
+  const _signMessage = async () => {
+    try {
+      console.log("signing message");
+      if (!publicKey) throw new WalletNotConnectedError();
+      const encodedMessage = new TextEncoder().encode(message);
+      const signature = await signMessage(encodedMessage);
+      setSignedMessage(encodedMessage);
+      setSignature(signature);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const verifyMessage = async () => {
+    try {
+      if (!publicKey) throw new WalletNotConnectedError();
+      const verified = nacl.sign.detached.verify(
+        signedMessage,
+        signature,
+        publicKey.toBuffer()
+      );
+      setVerified(verified);
+    } catch (error) {
+      setError(error);
+    }
+  };
+  return (
+    <>
+
+      <HomePage />
+
+
+
+    </>
   );
 }
 
