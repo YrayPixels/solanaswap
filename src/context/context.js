efab797765c1b20fe2b494368a6d80fb19922064
Moses Erhinyodavwe <54286496+YrayPixels@users.noreply.github.com>
2024-04-15 21:48:32 +0100

    Update context.js

--- src/context/context.js
+++ src/context/context.js
@@ -8,7 +8,7 @@ import { clusterApiUrl } from '@solana/web3.js';
 
 require("@solana/wallet-adapter-react-ui/styles.css");
 export default function Context() {
-    const network = WalletAdapterNetwork.Devnet;
+    const network = WalletAdapterNetwork.Mainnet;
 
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
